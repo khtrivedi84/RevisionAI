bd01a5e12b72e72e425723ddbff809773959129a
Kishan Trivedi <trivedikishan12345@gmail.com>
2023-07-11 20:09:09 -0700

    Uploading project to GitHub

---
+++ static/js/notes.js
@@ -0,0 +1,8 @@
+const openai = require('openai');
+import { Configuration, OpenAIApi } from 'openai';
+  var configuration = new Configuration({
+    apiKey: "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",
+  });
+  var openai = new OpenAIApi(configuration);
+
+alert("Hello")
